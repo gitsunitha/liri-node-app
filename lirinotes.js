402c4db3a5dfa62ae49b2c08005829cfaccb7a7a
gitsunitha <62200414+gitsunitha@users.noreply.github.com>
2020-06-11 14:03:26 -0400

    bug fixes

--- lirinotes.js
+++ lirinotes.js
@@ -5,7 +5,7 @@ var inquirer = require("inquirer");
 
 var inquirer1 = require("inquirer");
 
-var lirimodules = require("./liri.js")
+var lirimodules = require("./liri.js");
 
 // Create a "Prompt" with a series of questions.
 inquirer
@@ -16,73 +16,75 @@ inquirer
             message: "What would you like to search for?",
             choices: ["Movie", "Song", "Band", "DoWhatitSays"],
             name: "chooseAPI",
-            default: "Movies"
+            default: "Movies",
         },
-
     ])
     .then(function(inquirerResponse) {
         // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
         if (inquirerResponse.chooseAPI) {
-            inquirer1
-                .prompt([
-                    // User chooses what search he wants he wants .
-                    {
-                        type: "input",
-                        message: "What " + inquirerResponse.chooseAPI + " would you like to search for?",
-                        name: "nameToSearch",
-                        default: "Mr. Nobody",
-                        validate: async(input) => {
-                            if (!input) {
-                                return "Please enter a  " + inquirerResponse.chooseAPI + " to search";
-                            }
+            if (inquirerResponse.chooseAPI != "DoWhatitSays") {
+                inquirer1
+                    .prompt([
+                        // User chooses what search he wants he wants .
+                        {
+                            type: "input",
+                            message: "What " +
+                                inquirerResponse.chooseAPI +
+                                " would you like to search for?",
+                            name: "nameToSearch",
+                            default: "Mr. Nobody",
+                            validate: async(input) => {
+                                if (!input) {
+                                    return (
+                                        "Please enter a  " +
+                                        inquirerResponse.chooseAPI +
+                                        " to search"
+                                    );
+                                }
 
-                            return true;
+                                return true;
+                            },
+                        },
+                    ])
+                    .then(function(inquirerResponse1) {
+                        //console.log(inquirerResponse1.nameToSearch)
+                        // Here we know that we should get a search value
+                        switch (inquirerResponse.chooseAPI) {
+                            case "Movie":
+                                lirimodules.searchForMovies(inquirerResponse1.nameToSearch);
+
+                                break;
+                            case "Band":
+                                lirimodules.searchBandsInTown(inquirerResponse1.nameToSearch);
+                                break;
+                            case "Song":
+                                lirimodules.searchForSong(inquirerResponse1.nameToSearch);
+                                break;
+                            default:
+                                console.log("Invalid Case : Where did this come from?");
+                                break;
                         }
-                    },
-
-                ])
-                .then(function(inquirerResponse1) {
-                    //console.log(inquirerResponse1.nameToSearch)
-                    // Here we know that we should get a search value
-                    switch (inquirerResponse.chooseAPI) {
-                        case "Movie":
-                            lirimodules.searchForMovies(inquirerResponse1.nameToSearch)
-
-                            break;
-                        case "Band":
-                            lirimodules.searchBandsInTown(inquirerResponse1.nameToSearch)
-                            break;
-                        case "Song":
-                            lirimodules.searchForSong(inquirerResponse1.nameToSearch)
-                            break;
-                        case "DoWhatitSays":
-                            lirimodules.doWhatItSays(inquirerResponse1.nameToSearch)
-                            break;
-                        default:
-                            console.log("Invalid Case : Where did this come from?")
-                            break;
-                    }
-                })
-
+                    });
+            } else if (inquirerResponse.chooseAPI === "DoWhatitSays") {
+                lirimodules.doWhatItSays();
+            }
         } else {
             // should never reach this point.
-            console.log("Sorry you need to choose an input")
+            console.log("Sorry you need to choose an input");
         }
-
-    })
-
-
-function searchForSong(songName) {
-    var Spotify = require('node-spotify-api');
-    console.log("inside function")
-    var spotify = new Spotify({
-        id: "cb80a141ad0b4b55b55a29f07141d3aa",
-        secret: "fb2844b11cd7498aba8ddae06246a9c5"
     });
 
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
+function searchForSong(songName) {
+    var Spotify = require("node-spotify-api");
+    console.log("inside function");
+    var spotify = new Spotify({
+        id: "cb80a141ad0b4b55b55a29f07141d3aa",
+        secret: "fb2844b11cd7498aba8ddae06246a9c5",
+    });
+
+    spotify.search({ type: "track", query: songName }, function(err, data) {
         if (err) {
-            return console.log('Error occurred: ' + err);
+            return console.log("Error occurred: " + err);
         }
 
         console.log(JSON.stringify(data));
